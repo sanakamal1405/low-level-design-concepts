9d986e631560311440113da45982840c77bedce5
Sana Kamal <61350623+sanakamal1405@users.noreply.github.com>
2022-10-01 17:17:12 +0530

    Open/Close Principle Fixed

--- src/oops/SOLID/openClosePrinciple/before/employees/Intern.java
+++ src/oops/SOLID/openClosePrinciple/before/employees/Intern.java
@@ -1,8 +1,24 @@
 package oops.SOLID.openClosePrinciple.before.employees;
 
-public class Intern extends Employee {
+public class Intern extends Employee, TaxCalculator {
     public Intern(String fullName, int monthlyIncome, int nbHours) {
         super(fullName, monthlyIncome);
         setNbHoursPerWeek(nbHours);
+        INCOME_TAX_PERCENTAGE = 15;
+        
     }
+    
+    @Override
+    public double calculate(Employee employee) {
+        
+        if((employee.getMonthlyIncome() * 12) < 300000)
+            return 0.0d;
+        else{
+        return 
+                (employee.getMonthlyIncome() * PROFESSIONAL_TAX_PERCENTAGE) / 100 +
+                (employee.getMonthlyIncome() * INCOME_TAX_PERCENTAGE) / 100;}
+
+    }
+    
+    
 }
